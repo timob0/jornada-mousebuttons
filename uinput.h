8aeb69003338cc1593bc4f0a01a0d6308947e4b4
timob0 <43120425+timob0@users.noreply.github.com>
2021-01-18 21:00:01 +0100

    Added support for L/M/R buttons

--- uinput.h
+++ uinput.h
@@ -2,6 +2,16 @@
 #define UINPUT_H
 #include <libevdev/libevdev-uinput.h>
 
-struct libevdev_uinput *uinput_initialize();
+struct libevdev_uinput *uinput_initialize(struct libevdev **tsdevs);
+
 void uinput_send_right_click(struct libevdev_uinput *uinput);
+
+void uinput_send_right_btn(struct libevdev_uinput *uinput, int value);
+void uinput_send_middle_btn(struct libevdev_uinput *uinput, int value);
+void uinput_send_left_btn(struct libevdev_uinput *uinput, int value);
+
+void uinput_passthrough_event(const struct libevdev_uinput *uinput_dev,
+				unsigned int type,
+				unsigned int code,
+				int value);
 #endif
